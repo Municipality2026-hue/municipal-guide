cc2304e8f362b560b671ef3d86cf94cebaa8693a
Municipality2026-hue <aaishaalkharusi99@gmail.com>
2026-08-19 08:41:06 +0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'alsuwaiq-municipal-guide-v13';
+const OFFLINE_FILES = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './offline.html',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+
+  if (url.origin !== self.location.origin) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  const isPageRequest = event.request.mode === 'navigate' ||
+    url.pathname.endsWith('/app/') ||
+    url.pathname.endsWith('/app/index.html');
+
+  if (isPageRequest) {
+    event.respondWith(
+      fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./offline.html')))
+  );
+});
